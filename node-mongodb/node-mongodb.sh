349e5a02551ae0aa687f25243e7e3aeb71a56705
Sivabharathy <gotocva@gmail.com>
2020-01-12 16:32:56 +0530

    node-mongodb code updated

---
+++ node-mongodb/node-mongodb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo -i
+sudo apt-get update
+
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs
+
+echo "NodeJS installed successfully"
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 4B7C549A058F8B6B
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+sudo apt update
+sudo apt install mongodb-org
+sudo apt install mongodb-org=4.2.1 mongodb-org-server=4.2.1 mongodb-org-shell=4.2.1 mongodb-org-mongos=4.2.1 mongodb-org-tools=4.2.1
+sudo systemctl enable mongod
+sudo systemctl start mongod 
+mongod --version 
+
+echo "MongoDB installed successfully"
